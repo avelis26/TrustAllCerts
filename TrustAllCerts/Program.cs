a6125de6f8aa2ae8a1115ebf683e70e95bf811db
Graham Pinkston <graham.pinkston@gmail.com>
2018-04-12 15:07:33 -0500

    init

--- TrustAllCerts/Program.cs
+++ TrustAllCerts/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrustAllCerts
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy
 {
-	class Program
+	public bool CheckValidationResult(
+		ServicePoint srvPoint, X509Certificate certificate,
+		WebRequest request, int certificateProblem
+	)
 	{
-		static void Main(string[] args)
-		{
-		}
+		return true;
 	}
 }
